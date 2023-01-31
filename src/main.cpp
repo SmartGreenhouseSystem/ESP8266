0e535648e721a4c656ea5cd7e961f2a448f4b183
NikolaLugumerski <nikolalugumerski123@gmail.com>
2023-01-31 17:02:50 +0100

    Reading data from slave

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Websocket.hpp>
+#include <SoftwareSerial.h>
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include "../.config.h"
 
+#define RX D2
+#define TX D3
+
 Websocket websocket(SCADA_SERVER, SCADA_SERVER_ORIGIN);
+SoftwareSerial arduinoSerial (RX, TX);
 
 void setup() {
+	pinMode(RX, INPUT);
+	pinMode(TX, OUTPUT);
+
 	Serial.begin(115200);
+	arduinoSerial.begin(9600);
 	delay(100);
 
 	Serial.print("WiFi: Connecting to ");
 	Serial.print(WIFI_SSID);
+
+
 
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 	WiFi.softAPdisconnect(true);
@@ -27,9 +40,25 @@ void setup() {
 
 	websocket.connect();
 	websocket.subscribe("MicrocontrollerChannel", "1");
-	websocket.saveReading("Temp 1", 22.01);
+	
 }
 
 void loop() {
 	websocket.poll();
+
+	if(arduinoSerial.available() > 0)
+	{
+		std::string reading = arduinoSerial.readString().c_str(); 
+		std::vector<std::string> strings;
+		std::string buffer;
+		std::istringstream stream(reading);   
+    	while (std::getline(stream, buffer, '|')) {
+        strings.push_back(buffer);
+    	Serial.println(buffer.c_str());
+		}
+		
+		websocket.saveReading(strings[0], std::stof(strings[1].c_str()));
+	}
+
+
 }
